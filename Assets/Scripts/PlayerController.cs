e8505e2fe01611659c8b7eebb19868a1555e6c28
Sam Cordingley <samcordingley@gmail.com>
2016-02-19 21:34:16 +0000

    add player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpPower;
+    Rigidbody2D rigidbody;
+    Collider2D collider;
+    bool grounded = false;
+    // Use this for initialization
+    void Start () {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<Collider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collider2D collider)
+    {
+     
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+        rigidbody.velocity = new Vector2(speed * moveHorizontal, gameObject.GetComponent<Rigidbody2D>().velocity.y );
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigidbody.AddForce(new Vector2(0, jumpPower));
+        }
+
+     
+
+
+    }
+
+}
