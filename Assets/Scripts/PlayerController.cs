240f5d95d3539dc67d27a87e1c976546666cd1d1
Sam Cordingley <samcordingley@gmail.com>
2016-02-19 22:47:59 +0000

    add shield

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,18 @@ public class PlayerController : MonoBehaviour {
     public float jumpPower;
     Rigidbody2D rigidbody;
     Collider2D collider;
-
-    public BulletController bullet;
-
+    public GameObject shield;
     bool grounded = false;
     // Use this for initialization
     void Start () {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
         collider = gameObject.GetComponent<Collider2D>();
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("f"))
-        {
-            Instantiate(bullet, new Vector3(2f, 0.5f), Quaternion.identity);
-        }
+	    
 	}
 
     void OnCollisionEnter2D(Collider2D collider)
@@ -33,20 +29,27 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveShieldX = Input.GetAxis("RightX");
+        float moveShieldY = Input.GetAxis("RightY");
+
         
         rigidbody.velocity = new Vector2(speed * moveHorizontal, gameObject.GetComponent<Rigidbody2D>().velocity.y );
-
-
-        if (grounded && Input.GetButtonDown("Fire1"))
-        {
-            rigidbody.AddForce(new Vector2(0, jumpPower));
-        }
 
         if (collider.IsTouchingLayers())
             grounded = true;
         else
             grounded = false;
 
+        if (grounded && Input.GetAxis("Fire1") > 0.5f)
+        {
+            rigidbody.AddForce(new Vector2(0, jumpPower));
+        }
+
+        if (Mathf.Abs(moveShieldX) + Mathf.Abs(moveShieldY) >= 1)
+        {
+            Vector3 direction = new Vector3(-moveShieldX, moveShieldY, 0);
+            shield.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }
 
     }
 
