470d632385bf9491a735a11a6589c0c6e25ef7ca
Sam Cordingley <samcordingley@gmail.com>
2016-02-20 02:36:56 +0000

    fix shield jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,7 +43,8 @@ public class PlayerController : MonoBehaviour {
         float mousex = mousePos.x;
         float mousey = mousePos.y;
 
-        rigidbody.velocity = new Vector2(speed * moveHorizontal, gameObject.GetComponent<Rigidbody2D>().velocity.y );
+        if (moveHorizontal != 0)
+            rigidbody.velocity = new Vector2(speed * moveHorizontal, gameObject.GetComponent<Rigidbody2D>().velocity.y );
         sprite.transform.Rotate(new Vector3(0, 0, -2*speed * moveHorizontal));
         if (collider.IsTouchingLayers())
             grounded = true;
@@ -52,7 +53,7 @@ public class PlayerController : MonoBehaviour {
 
         if (grounded && Input.GetAxis("Fire1") > 0.5f)
         {
-            rigidbody.AddForce(new Vector2(0, jumpPower));
+            rigidbody.velocity = (new Vector2(0, jumpPower));
         }
 
         if (shield.GetComponent<ShieldController>().collision)
@@ -61,13 +62,23 @@ public class PlayerController : MonoBehaviour {
             Vector3 rot = shield.gameObject.transform.up;
             //Debug.Log(rot);
             
-            rigidbody.AddForce(rot*jumpPower);
+            rigidbody.velocity = (rot*jumpPower*2f);
         }
 
         if (Mathf.Abs(moveShieldX) + Mathf.Abs(moveShieldY) >= 1)
         {
             Vector3 direction = new Vector3(-moveShieldX, moveShieldY, 0);
-            shield.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+            Quaternion rot = Quaternion.LookRotation(Vector3.forward, direction);
+            if (!grounded)
+                shield.transform.rotation = rot;
+            else if (rot.eulerAngles.z < 90)
+                shield.transform.rotation = Quaternion.Euler(0,0,90);
+            else if (rot.eulerAngles.z > 270)
+                shield.transform.rotation = Quaternion.Euler(0, 0, 270);
+            else
+                shield.transform.rotation = rot;
+
+            Debug.Log(Quaternion.LookRotation(Vector3.forward, direction).eulerAngles);
         }
         /*
         Vector3 playerPos = Camera.main.WorldToScreenPoint(gameObject.transform.localPosition);
