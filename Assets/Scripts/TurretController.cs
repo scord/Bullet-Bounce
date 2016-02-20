ba17c0b0687075cf9da0b4a05e3b6eea170356d9
Sam Cordingley <samcordingley@gmail.com>
2016-02-20 15:02:43 +0000

    Assets and shit

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class TurretController : MonoBehaviour {
 
-    //public GameObject gun;
+    public GameObject gun;
     public BulletController bullet;
     public float repeatRate;
 	// Use this for initialization
 	void Start () {
-        //gun = transform.Find("Gun").gameObject;
+        gun = transform.Find("Gun").gameObject;
 
-        // gun.GetComponent<Animator>().Play("Idle");
+         gun.GetComponent<Animator>().Play("Idle");
 
         InvokeRepeating("Shoot", 2f, repeatRate);
 	}
 
     void Shoot ()
     {
+        gun.GetComponent<Animator>().Play("Turret Animation");
         //gun.GetComponent<Animator>().Play("Turret Animation");
         Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
     }
@@ -24,6 +25,7 @@ public class TurretController : MonoBehaviour {
     // Update is called once per frame
 	void Update ()
     {
-        
-	}
+        Vector3 direction = Vector3.Cross(GameObject.FindGameObjectWithTag("Player").transform.position - gameObject.transform.position, Vector3.forward);
+        gun.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+    }
 }
