4334260ecd911af03989d96134eb77aab137b452
Sam Cordingley <samcordingley@gmail.com>
2016-02-20 04:12:27 +0000

    add animation

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+    public GameObject gun;
+	// Use this for initialization
+	void Start () {
+        gun = transform.Find("Gun").gameObject;
+        
+       // gun.GetComponent<Animator>().Play("Idle");
+	}
+
+    void Shoot ()
+    {
+        gun.GetComponent<Animator>().Play("Turret Animation");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
